ba5fcc2947802583ed1afefa0be85d396544693a
kamuridesu <myk.gata14@gmail.com>
2022-07-24 21:25:47 -0300

    comment test variable

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ from typing import Union, Literal
 ROOT_FILES_FOLDER = "./contents"
 AUTHORIZED: dict = {}
 _users: Union[str, None] = os.getenv('users')
-# if _users is None: raise SyntaxError("Users to be authorized not found in env variables")
-if _users is None: _users="kamuri:123"
+if _users is None: raise SyntaxError("Users to be authorized not found in env variables")
+# if _users is None: _users="kamuri:123"
 for user in _users.split(";"):
     user_and_password = user.split(":")
     AUTHORIZED[user_and_password[0]] = user_and_password[1]
